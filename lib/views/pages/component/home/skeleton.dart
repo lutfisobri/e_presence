3357036ece1759f05ab8ea99e9582d47dc8d1cc5
Muhammad Sulaiman <91898723+xinyang1234@users.noreply.github.com>
2022-12-25 19:43:20 +0700

    no_internet home

---
+++ lib/views/pages/component/home/skeleton.dart
@@ -0,0 +1,418 @@
+import 'package:app_presensi/resources/utils/skeleton.dart';
+import 'package:flutter/material.dart';
+
+class SkeletonHome extends StatelessWidget {
+  const SkeletonHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            left: 19,
+            right: 128,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 19,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            left: 282,
+            right: 19,
+            top: 5,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 14,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+
+        //list
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            left: 19,
+            right: 19,
+            top: 42,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6),
+          ),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 56.6,
+            color: Color.fromRGBO(240, 240, 240, 1),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 293.4,
+            left: 31.6,
+            top: 53.4,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 35,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 199,
+            left: 107,
+            top: 53.4,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 15,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 138,
+            left: 107,
+            top: 74.4,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 15,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+
+        //list
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            left: 19,
+            right: 19,
+            top: 111.2,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6),
+          ),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 56.6,
+            color: Color.fromRGBO(240, 240, 240, 1),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 293.4,
+            left: 31.6,
+            top: 121.4,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 35,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 199,
+            left: 107,
+            top: 121.4,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 15,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 138,
+            left: 107,
+            top: 144.4,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 15,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+
+        //list
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            left: 19,
+            right: 19,
+            top: 180.41,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6),
+          ),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 56.6,
+            color: Color.fromRGBO(240, 240, 240, 1),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 293.4,
+            left: 31.6,
+            top: 191.21,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 35,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 199,
+            left: 107,
+            top: 191.21,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 15,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 138,
+            left: 107,
+            top: 213.4,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 15,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            left: 15,
+            right: 146,
+            top: 264,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 20,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            left: 288,
+            right: 16,
+            top: 265,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 16,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+
+        //container
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 360,
+            top: 308,
+          ),
+          decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.20),
+                offset: Offset(0, 1),
+                blurRadius: 2,
+              ),
+            ],
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(6),
+              bottomLeft: Radius.circular(6),
+            ),
+          ),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 173,
+            color: Color.fromRGBO(255, 255, 255, 1),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 372,
+            top: 316.65,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 94,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 382,
+            top: 416,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 18,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+
+        //container
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            left: 56,
+            right: 98,
+            top: 308,
+          ),
+          decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.20),
+                offset: Offset(0, 1),
+                blurRadius: 2,
+              ),
+            ],
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(6),
+              bottomLeft: Radius.circular(6),
+            ),
+          ),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 173,
+            color: Color.fromRGBO(255, 255, 255, 1),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 107.6,
+            top: 316.65,
+            left: 68.6,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 94,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 130,
+            top: 416,
+            left: 68.6,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 18,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            right: 250,
+            top: 437,
+            left: 68.6,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 15,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+
+        //container
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            left: 316,
+            top: 308,
+          ),
+          decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.20),
+                offset: Offset(0, 1),
+                blurRadius: 2,
+              ),
+            ],
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(6),
+              bottomLeft: Radius.circular(6),
+            ),
+          ),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 173,
+            color: Color.fromRGBO(255, 255, 255, 1),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            top: 316.65,
+            left: 328.6,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 94,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            top: 416,
+            left: 328.6,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 18,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(
+            top: 437,
+            left: 328.6,
+          ),
+          decoration: const BoxDecoration(),
+          child: SkeletonContainer.square(
+            height: 15,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/views/pages/home.dart
+++ lib/views/pages/home.dart
@@ -5,12 +5,16 @@ import 'package:app_presensi/app/providers/user.dart';
 import 'package:app_presensi/resources/widgets/shared/notification.dart';
 import 'package:app_presensi/views/pages/component/home/absensi.dart';
 import 'package:app_presensi/views/pages/component/home/informasi_akademik.dart';
+import 'package:app_presensi/views/pages/component/home/noconnection.dart';
 import 'package:app_presensi/views/pages/component/home/null.dart';
+import 'package:app_presensi/views/pages/component/home/skeleton.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:ntp/ntp.dart';
 import 'package:provider/provider.dart';
+
+import '../../resources/widgets/shared/button.dart';
 
 class Home extends StatefulWidget {
   const Home({
@@ -24,6 +28,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   late StreamSubscription<InternetConnectionStatus> listener;
   bool isOnline = false;
+  bool isLoading = true;
 
   void hasConnect() async {
     listener = InternetConnectionChecker().onStatusChange.listen(
@@ -32,14 +37,15 @@ class _HomeState extends State<Home> {
           case InternetConnectionStatus.connected:
             setState(() {
               isOnline = true;
+              isLoading = false;
             });
             break;
           case InternetConnectionStatus.disconnected:
             if (!mounted) return;
             setState(() {
               isOnline = false;
+              isLoading = true;
             });
-            showDialogbox();
             break;
         }
       },
@@ -55,7 +61,8 @@ class _HomeState extends State<Home> {
     if (isOnline) {
       loadPresensi();
     } else {
-      showDialogbox();
+      isLoading = true;
+      // NoConnection();
     }
   }
 
@@ -89,6 +96,12 @@ class _HomeState extends State<Home> {
     super.initState();
     hasConnect();
     init();
+    Future.delayed(const Duration(seconds: 1), () {
+      if (!mounted) return;
+      setState(() {
+        isLoading = false;
+      });
+    });
   }
 
   @override
@@ -97,103 +110,160 @@ class _HomeState extends State<Home> {
     super.dispose();
   }
 
-  showDialogbox() {
-    showModal<String>(
-      context: context,
-      builder: (BuildContext contex) => CupertinoAlertDialog(
-        title: const Text("Peringatan"),
-        content: const Text("Tidak ada koneksi internet"),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () async {
-              Navigator.pop(context, 'cancel');
-              if (!mounted) return;
-              if (!isOnline) showDialogbox();
-            },
-            child: const Text("Tutup"),
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Consumer<PelajaranProvider>(
-              builder: (context, value, child) => Column(
+    return isLoading
+        ? SkeletonHome()
+        : isOnline
+            ? Stack(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: 20,
-                      right: 20,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text(
-                          "Mata Pelajaran",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black,
-                            fontWeight: FontWeight.w600,
-                            fontFamily: "IBMPlex",
-                          ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Consumer<PelajaranProvider>(
+                        builder: (context, value, child) => Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                left: 20,
+                                right: 20,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: const [
+                                  Text(
+                                    "Mata Pelajaran",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w600,
+                                      fontFamily: "IBMPlex",
+                                    ),
+                                  ),
+                                  Text(
+                                    "Hari Ini",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.black,
+                                        fontFamily: "Roboto"),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 12.6,
+                            ),
+                            value.listPresensi.isEmpty
+                                ? NullPresensi()
+                                : ContentPresensi(),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 12.9, left: 19, right: 19, bottom: 6),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: const [
+                                  Text(
+                                    "Informasi Akademik",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w600,
+                                        fontFamily: "IBMPlex"),
+                                  ),
+                                  Text(
+                                    "Terbaru",
+                                    style: TextStyle(
+                                        fontSize: 12.6,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.black,
+                                        fontFamily: "Roboto"),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            InformasiAkademik(),
+                          ],
                         ),
-                        Text(
-                          "Hari Ini",
-                          style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black,
-                              fontFamily: "Roboto"),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  const SizedBox(
-                    height: 12.6,
-                  ),
-                  value.listPresensi.isEmpty
-                      ? NullPresensi()
-                      : ContentPresensi(),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        top: 12.9, left: 19, right: 19, bottom: 6),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text(
-                          "Informasi Akademik",
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w600,
-                              fontFamily: "IBMPlex"),
-                        ),
-                        Text(
-                          "Terbaru",
-                          style: TextStyle(
-                              fontSize: 12.6,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black,
-                              fontFamily: "Roboto"),
-                        ),
-                      ],
-                    ),
-                  ),
-                  InformasiAkademik(),
                 ],
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
+              )
+            : Container(
+                color: Color.fromRGBO(250, 250, 250, 1),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 152,
+                    ),
+                    Container(
+                      width: 500,
+                      margin: const EdgeInsets.only(
+                        left: 1,
+                        right: 1,
+                      ),
+                      child: Image.asset(
+                        "assets/image/noInternet.png",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    Text(
+                      "Tidak Ada Koneksi Internet",
+                      style: TextStyle(
+                        color: Color.fromRGBO(114, 182, 108, 1),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w900,
+                        fontFamily: 'poppins',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      "Mohon Periksa Kembali Koneksi Internet Anda.",
+                      style: TextStyle(
+                        color: Color.fromRGBO(100, 97, 97, 1),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                        fontFamily: 'poppins',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 18,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 19, right: 19),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Button(
+                              onPres: () async {
+                                hasConnect();
+                              },
+                              minimunSize: const Size(248, 41),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(7),
+                              ),
+                              textStyle: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700,
+                              ),
+                              child: const Text("COBA LAGI"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
   }
 
   Future<DateTime> validationTime() async {
